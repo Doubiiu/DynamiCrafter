fix: add missing parentheses in lvlb_weights formula for x0 parameterization

lvdm/models/ddpm3d.py
@@ -174,7 +174,7 @@ class DDPM(pl.LightningModule):
             lvlb_weights = self.betas ** 2 / (
                         2 * self.posterior_variance * to_torch(alphas) * (1 - self.alphas_cumprod))
         elif self.parameterization == "x0":
-            lvlb_weights = 0.5 * np.sqrt(torch.Tensor(alphas_cumprod)) / (2. * 1 - torch.Tensor(alphas_cumprod))
+            lvlb_weights = 0.5 * np.sqrt(torch.Tensor(alphas_cumprod)) / (2. * (1 - torch.Tensor(alphas_cumprod)))
         elif self.parameterization == "v":
             lvlb_weights = torch.ones_like(self.betas ** 2 / (
                     2 * self.posterior_variance * to_torch(alphas) * (1 - self.alphas_cumprod)))